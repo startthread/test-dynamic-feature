9c0d1d87b9e52afc83a03863d4dde8719b0f7450
Prakash Joshi <prakashjoshi0101@gmail.com>
2020-04-28 09:43:06 -0700

    Add dynamic feature module and launch activity through DynamicNavHostFragment

---
+++ app/src/main/java/com/startthread/test/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.startthread.test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        getView()?.findViewById<Button>(R.id.button)?.setOnClickListener {
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).navigate(
+                R.id.action_to_dynamic_feature_activity
+            )
+        }
+    }
+}
---
+++ dynamicfeature/src/main/java/com/startthread/dynamicfeature/DynamicFeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.startthread.dynamicfeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DynamicFeatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic_feature)
+    }
+}
